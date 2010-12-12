df0f07300bc536d78a9a820fbc7546fd3a13066a
Andrei Bocan <zmaxor@gmail.com>
2010-12-12 23:49:14 +0200

    RLE. Omnom :3

--- first.erl
+++ first.erl
@@ -1,6 +1,6 @@
 -module(first).
 
--export([last/1, last_two/1, item_at/2, length/1, reverse/1, flatten/1, compress/1, pack/1]).
+-export([last/1, last_two/1, item_at/2, length/1, reverse/1, flatten/1, compress/1, pack/1, rle/1]).
 
 last([]) ->
   empty;
@@ -75,3 +75,15 @@ pack([Head|Tail], List) ->
   [List|pack(Tail, [Head])];
 pack([], List) ->
   List.
+
+rle(List) ->
+  rle(List, 0, empty).
+
+rle([Head|Tail], 0, empty) ->
+  rle(Tail, 1, Head);
+rle([Head|Tail], Reps, Head) ->
+  rle(Tail, Reps+1, Head);
+rle([Head|Tail], Reps, Other) ->
+  [{ Other, Reps }|rle(Tail, 1, Head)];
+rle([], Reps, Head) ->
+  [{ Head, Reps }].
